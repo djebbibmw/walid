8c918495b5953554ed337512e4e96145fd785642
djebbibmw <djebbibmw@yahoo.fr>
2025-05-02 20:32:36 +0100

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,42 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use frame_support::{construct_runtime, parameter_types, dispatch::DispatchResult};
+use frame_system::{self as system, pallet_prelude::*};
+use sp_runtime::traits::{BlakeTwo256, IdentifyAccount, Verify};
+use sp_runtime::MultiAddress;
+use pallet_balances::Call as BalancesCall;
+use sp_core::H256;
+
+#[frame_support::pallet]
+pub mod pallet {
+
+    use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call]
+        fn initialize_block(origin: OriginFor<T>) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
+            // تنفيذ أي معاملة تحتاج إليها هنا
+            Ok(())
+        }
+    }
+}
+
+#[frame_support::construct_runtime]
+pub struct Runtime;
+
+#[frame_system::pallet]
+pub mod pallet_runtime {
+    use frame_system::pallet_prelude::*;
+}
 
 fn main() {
-    println!("مرحبا بك في سلسلة البلوكشين الخاصة بك!");
+    // موجه لإعداد العقدة وتهيئة الشبكة
+    println!("إطلاق شبكة Substrate الخاصة بك!");
+    // يمكنك إضافة المزيد من المنطق الخاص بك هنا لتشغيل الشبكة
 }
