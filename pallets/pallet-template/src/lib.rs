6af6e1d5eced7d8546daf1babf459d65f67e10b4
djebbibmw <djebbibmw@yahoo.fr>
2025-05-02 20:37:39 +0100

    lib.rs

--- pallets/pallet-template/src/lib.rs
+++ pallets/pallet-template/src/lib.rs
@@ -7,6 +7,7 @@ pub mod pallet {
     use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
     use frame_support::sp_runtime::traits::CheckedSub;
+    use sp_std::vec::Vec;
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
@@ -14,14 +15,27 @@ pub mod pallet {
     #[pallet::config]
     pub trait Config: frame_system::Config {}
 
-    // تعريف التخزين
+    // تخزين البيانات
+    #[pallet::storage]
+    #[pallet::getter(fn get_balance)]
+    pub(super) type Balances<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, u64, OptionQuery>;
+
+    // تعريف الأحداث
+    #[pallet::event]
+    pub enum Event<T: Config> {
+        BalanceSet(T::AccountId, u64),
+        HelloMessage(T::AccountId),
+    }
+
+    // تنفيذ الدوال
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         // دالة say_hello
         #[pallet::weight(10_000)]
         pub fn say_hello(origin: OriginFor<T>) -> DispatchResult {
-            let _sender = ensure_signed(origin)?;
-            log::info!("تم تنفيذ نداء 'say_hello'");
+            let sender = ensure_signed(origin)?;
+            log::info!("تم تنفيذ نداء 'say_hello' من {:?}", sender);
+            Self::deposit_event(Event::<T>::HelloMessage(sender));
             Ok(())
         }
 
@@ -29,10 +43,15 @@ pub mod pallet {
         #[pallet::weight(10_000)]
         pub fn set_balance(origin: OriginFor<T>, balance: u64) -> DispatchResult {
             let sender = ensure_signed(origin)?;
+            ensure!(balance >= 0, "الرصيد يجب أن يكون عددًا موجبًا");
 
             // تخزين الرصيد للمستخدم
-            Balances::<T>::insert(sender, balance);
+            Balances::<T>::insert(sender.clone(), balance);
             log::info!("تم تحديث رصيد المستخدم: {:?}", sender);
+
+            // نشر الحدث
+            Self::deposit_event(Event::<T>::BalanceSet(sender, balance));
+
             Ok(())
         }
 
@@ -40,7 +59,7 @@ pub mod pallet {
         #[pallet::weight(10_000)]
         pub fn get_balance(origin: OriginFor<T>) -> DispatchResult {
             let sender = ensure_signed(origin)?;
-            match Balances::<T>::get(sender) {
+            match Balances::<T>::get(sender.clone()) {
                 Some(balance) => log::info!("رصيد المستخدم {:?}: {:?}", sender, balance),
                 None => log::info!("المستخدم {:?} ليس لديه رصيد", sender),
             }
@@ -48,8 +67,10 @@ pub mod pallet {
         }
     }
 
-    // تخزين البيانات
-    #[pallet::storage]
-    #[pallet::getter(fn get_balance)]
-    pub(super) type Balances<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, u64, OptionQuery>;
-            }
+    // تخصيص الصلاحيات
+    #[pallet::call]
+    pub fn ensure_root(origin: OriginFor<T>) -> DispatchResult {
+        let _who = ensure_root(origin)?;
+        Ok(())
+    }
+}
